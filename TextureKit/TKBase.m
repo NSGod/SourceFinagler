2134e33f81c6adc5d147e93555d496a78cd1184e
Mark Douma <mark@markdouma.com>
2011-11-15 02:27:51 +0000

    Adding TKBase to hold 2 convenience variables, TKYES and TKNO.
    git-svn-id: file:///Users/mdouma46/Developer/sourceFinagler/branches/2.0.3@28 c3d3028b-b5cd-4c35-8691-6fb8b2347b27

---
+++ TextureKit/TKBase.m
@@ -0,0 +1,22 @@
+//
+//  TKBase.m
+//  Texture Kit
+//
+//  Created by Mark Douma on 10/31/2011.
+//  Copyright (c) 2010-2011 Mark Douma LLC. All rights reserved.
+//
+
+
+#import <TextureKit/TKBase.h>
+
+
+NSNumber * TKYES = nil;
+NSNumber * TKNO = nil;
+
+
+__attribute__((constructor)) static void TKInitBase() {
+//	NSLog(@"TKInitBase()");
+	
+	TKYES = [[NSNumber numberWithBool:YES] retain];
+	TKNO = [[NSNumber numberWithBool:NO] retain];
+}
