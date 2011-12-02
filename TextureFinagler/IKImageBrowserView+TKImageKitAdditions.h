732af47b5b3018e6cbea562ee01d6faac1b91d27
Mark Douma <mark@markdouma.com>
2011-12-02 22:51:58 +0000

    Committing these 2 files so I can rename them.
    git-svn-id: file:///Users/mdouma46/Developer/sourceFinagler/branches/2.5@46 c3d3028b-b5cd-4c35-8691-6fb8b2347b27

---
+++ TextureFinagler/IKImageBrowserView+TKImageKitAdditions.h
@@ -0,0 +1,13 @@
+//
+//  IKImageBrowserView+TKImageKitAdditions.h
+//  Source Finagler
+//
+//  Created by Mark Douma on 12/2/2011.
+//  Copyright (c) 2011 Mark Douma LLC. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface IKImageBrowserView (TKImageKitAdditions)
+
+@end
---
+++ TextureFinagler/IKImageBrowserView+TKImageKitAdditions.m
@@ -0,0 +1,13 @@
+//
+//  IKImageBrowserView+TKImageKitAdditions.m
+//  Source Finagler
+//
+//  Created by Mark Douma on 12/2/2011.
+//  Copyright (c) 2011 Mark Douma LLC. All rights reserved.
+//
+
+#import "IKImageBrowserView+TKImageKitAdditions.h"
+
+@implementation IKImageBrowserView (TKImageKitAdditions)
+
+@end
