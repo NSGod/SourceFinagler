5689326839fa66a37ad846b8022417b9f5193116
Mark Douma <mark@markdouma.com>
2012-01-06 09:17:59 +0000

    Committing changes before refactoring to TKVMTMaterial.
    git-svn-id: file:///Users/mdouma46/Developer/sourceFinagler/branches/2.5@48 c3d3028b-b5cd-4c35-8691-6fb8b2347b27

--- TextureKit/TKMaterial.h
+++ TextureKit/TKMaterial.h
@@ -1,16 +1,37 @@
 //
 //  TKMaterial.h
-//  Source Finagler
+//  Texture Kit
 //
 //  Created by Mark Douma on 1/17/2011.
-//  Copyright 2011 Mark Douma LLC. All rights reserved.
+//  Copyright (c) 2010-2012 Mark Douma LLC. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/NSObject.h>
+#import <TextureKit/TextureKitDefines.h>
 
 
-@interface TKMaterial : NSObject {
+@class TKVMTNode;
 
+
+@interface TKMaterial : NSObject <NSCopying> {
+	TKVMTNode		*rootNode;
 }
+
++ (id)materialWithContentsOfFile:(NSString *)aPath error:(NSError **)outError;
++ (id)materialWithContentsOfURL:(NSURL *)URL error:(NSError **)outError;
++ (id)materialWithData:(NSData *)aData error:(NSError **)outError;
+
+- (id)initWithContentsOfFile:(NSString *)aPath error:(NSError **)outError;
+- (id)initWithContentsOfURL:(NSURL *)URL error:(NSError **)outError;
+- (id)initWithData:(NSData *)aData error:(NSError **)outError;
+
+
+- (NSDictionary *)dictionaryRepresentation;
+
+- (NSString *)stringRepresentation;
+
+
+@property (nonatomic, retain) TKVMTNode *rootNode;
+
 
 @end
--- TextureKit/TKMaterial.m
+++ TextureKit/TKMaterial.m
@@ -3,15 +3,74 @@
 //  Texture Kit
 //
 //  Created by Mark Douma on 1/17/2011.
-//  Copyright (c) 2010-2011 Mark Douma LLC. All rights reserved.
+//  Copyright (c) 2010-2012 Mark Douma LLC. All rights reserved.
 //
 
 #import <TextureKit/TKMaterial.h>
+#import <Cocoa/Cocoa.h>
+#import <TextureKit/TKVMTNode.h>
 
+
+#define TK_DEBUG 1
 
 @implementation TKMaterial
 
+@synthesize rootNode;
 
+
++ (id)materialWithContentsOfFile:(NSString *)aPath error:(NSError **)outError {
+	return [[[[self class] alloc] initWithContentsOfFile:aPath error:outError] autorelease];
+}
+
+
++ (id)materialWithContentsOfURL:(NSURL *)URL error:(NSError **)outError {
+	return [[(TKMaterial *)[[self class] alloc] initWithContentsOfURL:URL error:outError] autorelease];
+}
+
+
++ (id)materialWithData:(NSData *)aData error:(NSError **)outError {
+	return [[[[self class] alloc] initWithData:aData error:outError] autorelease];
+}
+
+
+- (id)initWithContentsOfFile:(NSString *)aPath error:(NSError **)outError {
+	return [self initWithContentsOfURL:[NSURL fileURLWithPath:aPath] error:outError];
+}
+
+
+- (id)initWithContentsOfURL:(NSURL *)URL error:(NSError **)outError {
+	return [self initWithData:[NSData dataWithContentsOfURL:URL] error:outError];
+}
+
+
+- (id)initWithData:(NSData *)aData error:(NSError **)outError {
+	NSParameterAssert(aData != nil);
+#if TK_DEBUG
+	NSLog(@"[%@ %@]", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+#endif
+	if ((self = [super init])) {
+		
+	}
+	return self;
+}
+
+- (id)copyWithZone:(NSZone *)zone {
+	return nil;
+}
+
+
+- (NSDictionary *)dictionaryRepresentation {
+	return nil;
+}
+
+
+- (NSString *)stringRepresentation {
+	return nil;
+}
 
 
 @end
+
+
+
+
