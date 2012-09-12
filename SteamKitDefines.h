d28b0aad69365a68c63e67434c4c937a6d147569
Mark Douma <mark@markdouma.com>
2012-09-12 15:05:11 +0000

    Bumped versions to 2.0.3 and copyright year to 2012.
    git-svn-id: file:///Users/mdouma46/Developer/sourceFinagler/branches/2.0.3@63 c3d3028b-b5cd-4c35-8691-6fb8b2347b27

--- SteamKitDefines.h
+++ SteamKitDefines.h
@@ -3,7 +3,7 @@
 //  Steam Kit
 //
 //  Created by Mark Douma on 12/17/2010.
-//  Copyright (c) 2010-2011 Mark Douma LLC. All rights reserved.
+//  Copyright (c) 2010-2012 Mark Douma LLC. All rights reserved.
 //
 
 #ifndef _STEAMKITDEFINES_H
