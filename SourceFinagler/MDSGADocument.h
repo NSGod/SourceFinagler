7a42683a9bcf73ace88043d6002ba7e3a2d505fe
Mark Douma <mark@markdouma.com>
2012-10-05 22:56:57 +0000

    Added MDSGADocument for new SGA files.
    git-svn-id: file:///Users/mdouma46/Developer/sourceFinagler/branches/2.0.3@109 c3d3028b-b5cd-4c35-8691-6fb8b2347b27

---
+++ SourceFinagler/MDSGADocument.h
@@ -0,0 +1,16 @@
+//
+//  MDSGADocument.h
+//  Source Finagler
+//
+//  Created by Mark Douma on 9/10/2010.
+//  Copyright © 2010-2011 Mark Douma LLC. All rights reserved.
+//
+
+#import "MDHLDocument.h"
+
+
+@interface MDSGADocument : MDHLDocument {
+
+}
+
+@end
