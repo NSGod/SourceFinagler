5a1fa6d74832b174c43264696314874763d2940c
Mark Douma <mark@markdouma.com>
2013-07-29 20:36:12 +0000

    Removed TKSFTIImageRep.h and added additional TKBase.h.
    git-svn-id: file:///Users/mdouma46/Developer/sourceFinagler/branches/2.0.3@152 c3d3028b-b5cd-4c35-8691-6fb8b2347b27

--- TextureKit/TextureKit.h
+++ TextureKit/TextureKit.h
@@ -3,13 +3,13 @@
 //  Texture Kit
 //
 //  Created by Mark Douma on 12/17/2010.
-//  Copyright (c) 2010-2011 Mark Douma LLC. All rights reserved.
+//  Copyright (c) 2010-2013 Mark Douma LLC. All rights reserved.
 //
 
 #import <TextureKit/TextureKitDefines.h>
+#import <TextureKit/TKBase.h>
 #import <TextureKit/TKImage.h>
 #import <TextureKit/TKImageRep.h>
 #import <TextureKit/TKDDSImageRep.h>
 #import <TextureKit/TKVTFImageRep.h>
-#import <TextureKit/TKSFTIImageRep.h>
 
