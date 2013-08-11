ce398722e9f85c20517f49a009ee0dea58877200
Mark Douma <mark@markdouma.com>
2013-08-11 22:58:08 +0000

    Updated Copyright to 2012.
    Added MDSGADocument to the -documentClassNames method.
    
    
    git-svn-id: file:///Users/mdouma46/Developer/sourceFinagler/branches/2.5@184 c3d3028b-b5cd-4c35-8691-6fb8b2347b27

--- TextureFinagler/TKDocumentController.h
+++ TextureFinagler/TKDocumentController.h
@@ -3,7 +3,7 @@
 //  Texture Kit
 //
 //  Created by Mark Douma on 3/16/2009.
-//  Copyright (c) 2009-2011 Mark Douma. All rights reserved.
+//  Copyright (c) 2009-2012 Mark Douma. All rights reserved.
 //
 
 #import <AppKit/NSDocumentController.h>
