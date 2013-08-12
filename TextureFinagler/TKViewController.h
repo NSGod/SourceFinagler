f25b803ba9f492877c6edee808424df962384014
Mark Douma <mark@markdouma.com>
2013-08-12 00:14:41 +0000

    Changed to be a subclass of NSViewController.
    This subclass of NSViewController makes it behave more like UIViewController on iOS: it will automatically try loading the nib file by the same name as the subclass.
    
    Also handles autosaving of view size at application quit and restoring view size on app launch.
    
    git-svn-id: file:///Users/mdouma46/Developer/sourceFinagler/branches/2.5@197 c3d3028b-b5cd-4c35-8691-6fb8b2347b27

--- TextureFinagler/TKViewController.h
+++ TextureFinagler/TKViewController.h
@@ -1,68 +1,39 @@
 //
 //  TKViewController.h
-//  Source Finagler
+//  Procon Finagler
 //
-//  Created by Mark Douma on 4/14/2011.
-//  Copyright 2011 Mark Douma LLC. All rights reserved.
+//  Created by Mark Douma on 6/9/2012.
+//  Copyright (c) 2012 Mark Douma LLC. All rights reserved.
 //
 
 #import <Cocoa/Cocoa.h>
 
-
-extern NSString * const TKViewControllerViewDidChangeNotification;
-
-
-@interface TKViewController : NSResponder {
+@interface TKViewController : NSViewController {
 	
-	IBOutlet NSWindow		*window;	// non-retained
-    IBOutlet NSView			*view;
+	NSSize					minWinSize;
+	NSSize					maxWinSize;
 	
-    NSString				*nibName;
-    NSBundle				*nibBundle;
-    NSString				*title;
-
-	NSSize					minSize;
-	NSSize					maxSize;
-	BOOL					isResizable;
+	BOOL					resizable;
 }
 
-- (id)initWithWindow:(NSWindow *)aWindow;
+@property (nonatomic, assign) NSSize minWinSize;
+@property (nonatomic, assign) NSSize maxWinSize;
 
-/* The designated initializer. The specified nib should typically have the class of the file's owner set to NSViewController, or a subclass of yours, with the "view" outlet connected to a view. If you pass in a nil nib name then -nibName will return nil and -loadView will throw an exception; you most likely must also invoke -setView: before -view is invoked, or override -loadView. If you pass in a nil bundle then -nibBundle will return nil and -loadView will interpret it using the same rules as -[NSNib initWithNibNamed:bundle:].
-*/
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil;
-
-
-/* The localized title of the view. This class doesn't actually do anything with the value of this property other than hold onto it, and be KVC and KVO compliant for "title." The default implementation of -setTitle: copies the passed-in object ("title" is an attribute). This property is here because so many anticipated uses of this class will involve letting the user choose among multiple named views using a pulldown menu or something like that.
-*/
-
-@property (copy) NSString *title;
+@property (nonatomic, assign, getter=isResizable) BOOL resizable;
 
 
-/* Return the view. The default implementation of this method first invokes [self loadView] if the view hasn't been set yet.
-*/
-- (NSView *)view;
+- (void)didSwitchToView:(id)sender;
 
-/* Instantiate the view and then set it. The default implementation of this method invokes [self nibName] and [self nibBundle] and then uses the NSNib class to load the nib with this object as the file's owner. If the "view" outlet of the file's owner in the nib is properly connected, the regular nib loading machinery will send this object a -setView: message. You can override this method to customize how nib loading is done, including merely adding new behavior immediately before or immediately after nib loading done by the default implementation. You should not invoke this method from other objects unless you take care to avoid redundant invocations; NSViewController's default implement can handle them but overrides in subclasses might not. (Typically other objects should instead invoke -view and let the view controller do whatever it takes to fulfill the request.)
-*/
-- (void)loadView;
+- (void)cleanup;
 
-/* Return the name of the nib to be loaded to instantiate the view, and the bundle from which to load it. The default implementations of these merely return whatever value was passed to the initializer.
-*/
-- (NSString *)nibName;
-- (NSBundle *)nibBundle;
+- (NSString *)viewControllerViewSizeAutosaveString;
 
-/* Set the view. You can invoke this method immediately after creating the object to specify a view that's created in a different manner than what -view's default implementation would do.
-*/
-- (void)setView:(NSView *)aView;
+- (NSString *)viewSizeAutosaveName;
+
+- (void)viewDidLoad;
 
 
-- (void)viewControllerDidLoadNib;
-
-
-- (void)viewDidChange:(NSNotification *)notification;
-
-- (NSWindow *)window;
++ (NSSize)windowSizeForViewWithSize:(NSSize)size;
 
 @end
 
--- TextureFinagler/TKViewController.m
+++ TextureFinagler/TKViewController.m
@@ -1,190 +1,145 @@
 //
 //  TKViewController.m
-//  Source Finagler
+//  Procon Finagler
 //
-//  Created by Mark Douma on 4/14/2011.
-//  Copyright 2011 Mark Douma LLC. All rights reserved.
+//  Created by Mark Douma on 6/9/2012.
+//  Copyright (c) 2012 Mark Douma LLC. All rights reserved.
 //
 
 #import "TKViewController.h"
-#import "TKAppKitAdditions.h"
 
 
-#pragma mark controller
-#define MD_DEBUG 0
+#define PC_DEBUG 1
 
 
-NSString * const TKViewControllerViewDidChangeNotification	= @"TKViewControllerViewDidChange";
-
-
-@interface TKViewController (MDPrivate)
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil window:(NSWindow *)aWindow;
-@end
+#define TK_WINDOW_TITLEBAR_HEIGHT 22.0
 
 
 @implementation TKViewController
 
-@dynamic title;
+@synthesize minWinSize;
+@synthesize maxWinSize;
+@synthesize resizable;
 
 
 - (id)init {
-	return [self initWithNibName:[self nibName] bundle:[self nibBundle]];
-}
-
-- (id)initWithWindow:(NSWindow *)aWindow {
-	return [self initWithNibName:[self nibName] bundle:[self nibBundle] window:aWindow];
-}
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-	return [self initWithNibName:nibNameOrNil bundle:nibBundleOrNil window:nil];
-}
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil window:(NSWindow *)aWindow {
-#if MD_DEBUG
-	NSLog(@"[%@ %@]", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
-#endif
-	if ((self = [super init])) {
-		window = aWindow;
-		nibName = [nibNameOrNil retain];
-		nibBundle = [nibBundleOrNil retain];
+	if ((self = [super initWithNibName:NSStringFromClass([self class]) bundle:nil])) {
+		resizable = YES;
 	}
 	return self;
 }
 
-
-- (void)dealloc {
-#if MD_DEBUG
-	NSLog(@"[%@ %@] (TKViewController)", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
-#endif
-	window = nil;
-	[view release];
-	[nibName release];
-	[nibBundle release];
-	[title release];
-	[super dealloc];
-}
-
-- (void)awakeFromNib {
-#if MD_DEBUG
-	NSLog(@"[%@ %@] view == %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), view);
-#endif
-	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(viewDidChange:) name:TKViewControllerViewDidChangeNotification object:view];
++ (NSSize)windowSizeForViewWithSize:(NSSize)size {
+	return NSMakeSize(size.width, size.height + TK_WINDOW_TITLEBAR_HEIGHT);
 }
 
 
-- (void)viewControllerDidLoadNib {
-#if MD_DEBUG
-	NSLog(@"[%@ %@] (TKViewController)", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+- (void)didSwitchToView:(id)sender {
+#if PC_DEBUG
+	NSLog(@"[%@ %@]", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+#endif
+	
+	if (NSEqualSizes(minWinSize, NSZeroSize) && NSEqualSizes(maxWinSize, NSZeroSize)) {
+		NSRect viewFrame = self.view.frame;
+		if (resizable) {
+			minWinSize = [TKViewController windowSizeForViewWithSize:viewFrame.size];
+			maxWinSize = NSMakeSize(FLT_MAX, FLT_MAX);
+		} else {
+			minWinSize = [TKViewController windowSizeForViewWithSize:viewFrame.size];
+			maxWinSize = [TKViewController windowSizeForViewWithSize:viewFrame.size];
+		}
+	}
+	
+	[self.view.window setShowsResizeIndicator:resizable];
+	
+	[self.view.window setMinSize:minWinSize];
+	[self.view.window setMaxSize:maxWinSize];
+	
+	[[self.view.window standardWindowButton:NSWindowZoomButton] setEnabled:resizable];
+	
+}
+	
+	
+- (NSString *)viewControllerViewSizeAutosaveString {
+	return [NSString stringWithFormat:@"TKViewController viewSize %@", self.viewSizeAutosaveName];
+}
+
+
+- (void)cleanup {
+#if PC_DEBUG
+	NSLog(@"[%@ %@]", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+#endif
+	
+	if (self.viewSizeAutosaveName) {
+		NSString *existingSize = [[NSUserDefaults standardUserDefaults] objectForKey:[self viewControllerViewSizeAutosaveString]];
+		NSString *currentSize = NSStringFromSize(self.view.frame.size);
+		
+#if PC_DEBUG
+		NSLog(@"[%@ %@] existingSize == %@, currentSize == %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), existingSize, currentSize);
+#endif
+
+		
+		[[NSUserDefaults standardUserDefaults] setObject:currentSize forKey:[self viewControllerViewSizeAutosaveString]];
+		
+		NSString *newSavedSize = [[NSUserDefaults standardUserDefaults] objectForKey:[self viewControllerViewSizeAutosaveString]];
+		
+#if PC_DEBUG
+		NSLog(@"[%@ %@] ******* SAVING %@'s viewSize == %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), [self viewSizeAutosaveName], newSavedSize);
+#endif
+
+	}
+}
+
+- (NSString *)viewSizeAutosaveName {
+	return nil;
+}
+
+- (void)viewDidLoad {
+#if PC_DEBUG
+	NSLog(@"[%@ %@]", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
 #endif
 	
 }
 
 
-/* Instantiate the view and then set it. The default implementation of this method invokes [self nibName] and [self nibBundle] and then uses the NSNib class to load the nib with this object as the file's owner. If the "view" outlet of the file's owner in the nib is properly connected, the regular nib loading machinery will send this object a -setView: message. You can override this method to customize how nib loading is done, including merely adding new behavior immediately before or immediately after nib loading done by the default implementation. You should not invoke this method from other objects unless you take care to avoid redundant invocations; NSViewController's default implement can handle them but overrides in subclasses might not. (Typically other objects should instead invoke -view and let the view controller do whatever it takes to fulfill the request.)
-*/
 - (void)loadView {
-#if MD_DEBUG
-	NSLog(@"[%@ %@] (TKViewController)", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+#if PC_DEBUG
+	NSLog(@"[%@ %@]", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
 #endif
-	if (view == nil) {
-		if (nibName) {
-			if (![NSBundle loadNibNamed:nibName owner:self]) {
-				[NSBundle runFailedNibLoadAlert:nibName];
-			}
+	[super loadView];
+	
+	if (self.viewSizeAutosaveName) {
+//		NSString *key = [NSString stringWithFormat:@"TKViewController viewSize %@", [self viewSizeAutosaveName]];
+		
+		NSString *savedSize = [[NSUserDefaults standardUserDefaults] objectForKey:[self viewControllerViewSizeAutosaveString]];
+		
+		if (savedSize == nil) {
+			[[NSUserDefaults standardUserDefaults] setObject:NSStringFromSize(self.view.frame.size) forKey:[self viewControllerViewSizeAutosaveString]];
 		}
-		[view release];
-		[self viewControllerDidLoadNib];
-	}
-}
-
-
-/* Return the view. The default implementation of this method first invokes [self loadView] if the view hasn't been set yet.
-*/
-- (NSView *)view {
-#if MD_DEBUG
-	NSLog(@"[%@ %@] (TKViewController)", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+		
+//		if ([[NSUserDefaults standardUserDefaults] objectForKey:key] == nil) {
+//			[[NSUserDefaults standardUserDefaults] setObject:NSStringFromSize(self.view.frame.size) forKey:key];
+//		}
+		
+#if PC_DEBUG
+//		NSLog(@"[%@ %@] self.view == %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), self.view);
+		NSLog(@"[%@ %@] self.view.frame == %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), NSStringFromRect(self.view.frame));
+		NSLog(@"[%@ %@] ******* RESTORING %@'s viewSize == %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), [self viewSizeAutosaveName], [[NSUserDefaults standardUserDefaults] objectForKey:[self viewControllerViewSizeAutosaveString]]);
 #endif
-	if (view == nil) [self loadView];
-	return view;
-}
-
-/* Set the view. You can invoke this method immediately after creating the object to specify a view that's created in a different manner than what -view's default implementation would do.
-*/
-- (void)setView:(NSView *)aView {
-#if MD_DEBUG
-	NSLog(@"[%@ %@] (TKViewController)", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
-#endif
-	[aView retain];
-	[view release];
-	view = aView;
-}
-
-
-- (void)viewDidChange:(NSNotification *)notification {
-	if ([notification object] == view) {
-#if MD_DEBUG
-	NSLog(@"[%@ %@] (TKViewController)", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
+		[self.view setFrameSize:NSSizeFromString([[NSUserDefaults standardUserDefaults] objectForKey:[self viewControllerViewSizeAutosaveString]])];
+		
+//		[self.view setFrameSize:NSSizeFromString([[NSUserDefaults standardUserDefaults] objectForKey:key])];
+		
+#if PC_DEBUG
+		NSLog(@"[%@ %@] self.view.frame == %@", NSStringFromClass([self class]), NSStringFromSelector(_cmd), NSStringFromRect(self.view.frame));
 #endif
 		
-		if (isResizable) {
-			
-			[window setShowsResizeIndicator:YES];
-			
-			[window setMinSize:minSize];
-			[window setMaxSize:maxSize];
-			
-			[[window standardWindowButton:NSWindowZoomButton] setEnabled:YES];
-
-		} else {
-			
-			NSSize localMinSize = NSZeroSize;
-			NSSize localMaxSize = NSZeroSize;
-			
-			NSRect viewFrame = [view frame];
-			
-			localMinSize = NSMakeSize(NSWidth(viewFrame), NSHeight(viewFrame) + 22.0);
-			localMaxSize = NSMakeSize(NSWidth(viewFrame), NSHeight(viewFrame) + 22.0);
-			
-			[window setShowsResizeIndicator:NO];
-			
-			[window setMinSize:localMinSize];
-			[window setMaxSize:localMaxSize];
-			
-			[[window standardWindowButton:NSWindowZoomButton] setEnabled:NO];
-			
-		}
 	}
+	[self viewDidLoad];
 }
 
-/* Return the name of the nib to be loaded to instantiate the view, and the bundle from which to load it. The default implementations of these merely return whatever value was passed to the initializer.
-*/
-- (NSString *)nibName {
-	NSLog(@"[%@ %@] subclass should override!", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
-	return nibName;
-}
-
-
-- (NSBundle *)nibBundle {
-//	NSLog(@"[%@ %@] subclass should override!", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
-	return nibBundle;
-}
-
-/* The localized title of the view. This class doesn't actually do anything with the value of this property other than hold onto it, and be KVC and KVO compliant for "title." The default implementation of -setTitle: copies the passed-in object ("title" is an attribute). This property is here because so many anticipated uses of this class will involve letting the user choose among multiple named views using a pulldown menu or something like that.
- */
-- (void)setTitle:(NSString *)aTitle {
-	NSString *copy = [aTitle copy];
-	[title release];
-	title = copy;
-}
-
-
-- (NSString *)title {
-	NSLog(@"[%@ %@] subclass should override!", NSStringFromClass([self class]), NSStringFromSelector(_cmd));
-	return title;
-}
-
-- (NSWindow *)window {
-	return window;
-}
 
 @end
+
+
+
