bca59dfc52de4d9ba655e543a67b7988b042c61c
Mark Douma <mark@markdouma.com>
2013-08-12 03:57:41 +0000

    I originally created numerous OpenGL-related classes in an attempt to implement something like iOS's GLKit for Mac OS X. Had mixed results. These classes will be removed in favor of the actual GLKit.framework and SceneKit.framework added in 10.8.
    git-svn-id: file:///Users/mdouma46/Developer/sourceFinagler/branches/2.5@200 c3d3028b-b5cd-4c35-8691-6fb8b2347b27

--- TextureKit/TextureKit.h
+++ TextureKit/TextureKit.h
@@ -13,34 +13,6 @@
 #import <TextureKit/TKDDSImageRep.h>
 #import <TextureKit/TKVTFImageRep.h>
 
-#import <TextureKit/TKOpenGLBase.h>
-#import <TextureKit/TKOpenGLObject.h>
-#import <TextureKit/TKOpenGLDrawable.h>
-
-
-#import <TextureKit/TKTexture.h>
-#import <TextureKit/TKDDSTexture.h>
-#import <TextureKit/TKVTFTexture.h>
-
-#import <TextureKit/TKOpenGLView.h>
-#import <TextureKit/TKOpenGLRenderer.h>
-#import <TextureKit/TKShaderProgram.h>
-#import <TextureKit/TKShader.h>
-
-#import <TextureKit/TKMathBase.h>
-#import <TextureKit/TKMatrix3.h>
-#import <TextureKit/TKMatrix4.h>
-#import <TextureKit/TKVector2.h>
-#import <TextureKit/TKVector3.h>
-#import <TextureKit/TKVector4.h>
-#import <TextureKit/TKQuaternion.h>
-
-
-#import <TextureKit/TKModel.h>
-#import <TextureKit/TKOBJModel.h>
-#import <TextureKit/TKOBJParts.h>
-
-
 #import <TextureKit/TKVMTMaterial.h>
 #import <TextureKit/TKVMTNode.h>
 
