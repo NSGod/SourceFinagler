c201b6fc7f8b514d9034e10464f3a8d5334a774b
Mark Douma <mark@markdouma.com>
2013-11-18 14:58:38 -0500

    Updated to HLLib 2.4.4. This should fix issue with .vkp files.
    Conflicts:
    	HL/HL.h
    	HL/HLInfo.plist

--- HL/HL.h
+++ HL/HL.h
@@ -7,7 +7,8 @@
 //
 //	HLLib
 //	Copyright (c) 2006-2013 Ryan Gregg
-
+//  http://nemesis.thewavelength.net
+//  
 //	This library is free software; you can redistribute it and/or
 //	modify it under the terms of the GNU Lesser General Public
 //	License as published by the Free Software Foundation; either
