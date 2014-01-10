ce90fed2141cda3f654a47dd5183002b57e0f886
Mark Douma <mark@markdouma.com>
2014-01-10 01:51:02 -0500

    HLKit: Updated and enhanced the `HKVPKFile` class.
    Added an `HKVPKArchiveType` typedef to differentiate between the different types of VPK files (Source Addon, _dir.vpk file, or multi-part archive).
    
    Added an `HKSourceAddonStatus` typedef to represent the status of the archive if it’s an `HKVPKSourceAddonFileArchiveType` type.
    
    Added `archiveVersion`, `archiveCount`, `archiveDirectoryFilePath`, `sourceAddonStatus`, and `sourceAddonID` properties. These will make `HKVPKFile` be more self-contained with respect to the Source Addon feature (previously, it put an undue burden on `VSSteamManager`.
    
    Enhanced the `description` method with new properties being printed.

--- HLKit/HKVPKFile.h
+++ HLKit/HKVPKFile.h
@@ -9,13 +9,54 @@
 #import <HLKit/HKArchiveFile.h>
 
 
-@interface HKVPKFile : HKArchiveFile {
+enum {
+	HKVPKUnknownArchiveType				= 0,
+	HKVPKSourceAddonFileArchiveType		= 1,
+	HKVPKDirectoryArchiveType			= 2,	// blah_dir.vpk
+	HKVPKMultipartArchiveType			= 3,	// blah_001.vpk
+};
+typedef NSUInteger HKVPKArchiveType;
 
-	NSUInteger		archiveCount;
-	NSUInteger		addonGameID;
+
+enum {
+	HKSourceAddonStatusUnknown	= 0,
+	HKSourceAddonNotAnAddonFile,
+	HKSourceAddonNoAddonInfoFound,
+	HKSourceAddonAddonInfoUnreadable,
+	HKSourceAddonNoGameIDFoundInAddonInfo,
+	HKSourceAddonValidAddon
+};
+typedef NSUInteger HKSourceAddonStatus;
+
+
+@interface HKVPKFile : HKArchiveFile {
+	
+	NSUInteger				archiveVersion;
+	NSUInteger				archiveCount;
+	
+	NSString				*archiveDirectoryFilePath;
+	
+	HKVPKArchiveType		vpkArchiveType;
+	
+	HKSourceAddonStatus		sourceAddonStatus;
+	
+	NSUInteger				sourceAddonGameID;
+	
 }
 
+@property (nonatomic, readonly, assign) NSUInteger archiveVersion;
 @property (nonatomic, readonly, assign) NSUInteger archiveCount;
-@property (nonatomic, readonly, assign) NSUInteger	addonGameID;
+
+@property (nonatomic, readonly, retain) NSString *archiveDirectoryFilePath;
+
+@property (nonatomic, readonly, assign) HKVPKArchiveType vpkArchiveType;
+
+@property (nonatomic, readonly, assign) HKSourceAddonStatus sourceAddonStatus;
+
+@property (nonatomic, readonly, assign) NSUInteger sourceAddonGameID;
+
+
 
 @end
+
+
